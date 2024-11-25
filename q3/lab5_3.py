a5c6d19c9ba09d3db71534b3bc276b0ad50422b2
christy-josephanton <109394349+christy-josephanton@users.noreply.github.com>
2024-11-25 11:46:25 -0500

    add q3 WIP (not tested)

---
+++ q3/lab5_3.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#PARAMETERS
+arrival_rate = 100
+output_rate = 1000
+sim_time = 1000
+queue_sizes = range(1,500)  # Queue sizes from 1 to 1000
+token_bucket_max = 100  # token bucket limit
+token_rate = 500  # token/sec
+#PARAMETERS
+
+
+packet_loss_rates = []
+mean_output_rates = []
+
+# run for each queue size
+for data_bucket_max in queue_sizes:
+
+    total_ticks = int(output_rate * sim_time)  # total clock ticks
+    clock_interval = 1 / output_rate           # clock tick time
+    avg_arrival_interval = 1 / arrival_rate   # mean inter-arrival time
+    token_interval = 1 / token_rate  
+
+    # keeps track of num elements in queue
+    data_bucket = 0 
+    token_bucket = 0 
+
+    packets_lost = 0
+    tokens_lost = 0
+    packets_transmitted = 0
+    total_arrivals = 0
+    total_tokens = 0
+    time = 0
+
+    next_arrival = np.random.exponential(avg_arrival_interval)  # first packet arrival
+    next_token = 0 # first token arrival
+
+    for tick in range(total_ticks):
+    
+        time += clock_interval
+
+        #schedule token
+        while next_token <= time:
+            total_tokens+=1
+            if token_bucket < token_bucket_max:
+                token_bucket += 1
+            else:
+                tokens_lost += 1
+            next_token += token_interval
+
+
+        # process all arrival events between last clock tick and this clock tick
+        while next_arrival <= time:
+            #new arrival
+            total_arrivals += 1
+
+            if data_bucket < data_bucket_max: 
+                #add to queue if theres space
+                data_bucket += 1
+
+            else:
+                #dont add to queue if theres no space, keep track of dropped packet
+                packets_lost += 1
+    
+            # schedule next arrival
+            next_arrival += np.random.exponential(avg_arrival_interval)  
+
+        # transmit every clock tick if available
+        if data_bucket > 0 and token_bucket > 0:
+            data_bucket -= 1
+            token_bucket -=1
+            packets_transmitted += 1
+
+    # Calculate metrics
+    
+    packet_loss_rate = packets_lost / total_arrivals
+    mean_output_rate = packets_transmitted / sim_time
+
+    packet_loss_rates.append(packet_loss_rate)
+    mean_output_rates.append(mean_output_rate)
+
+
+plt.figure(figsize=(10, 8))
+plt.plot(queue_sizes, packet_loss_rates, color='blue')
+plt.title("Packet Loss Rate vs Queue Size")
+plt.xlabel("Queue Size (B)")
+plt.ylabel("Packet Loss Rate")
+plt.grid()
+plt.savefig('5_1a_loss_rate.png')
+
+plt.clf() 
+
+plt.figure(figsize=(10, 8))
+plt.plot(queue_sizes, mean_output_rates, color='orange')
+plt.title("Mean Output Rate vs Queue Size")
+plt.xlabel("Queue Size (B)")
+plt.ylabel("Mean Output Rate (packets/s)")
+plt.grid()
+plt.savefig('5_1a_out_rate.png')
